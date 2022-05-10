3a500d4a52c3a5a8ec0e817a311be3b125a2e500
khuongduy354 <khuongduy354@gmail.com>
2022-05-10 21:41:14 +0700

    v1 instead of big path

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,6 @@ import { Application } from "express";
 import ProductRouter from "./product.route";
 import UserRouter from "./user.route";
 export const ConfigRoute = (app: Application) => {
-  app.use("/product", ProductRouter);
-  app.use("/user", UserRouter);
+  app.use("/v1", ProductRouter);
+  app.use("/v1", UserRouter);
 };
